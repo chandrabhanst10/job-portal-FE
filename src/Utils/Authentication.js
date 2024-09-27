0566c6a69ead882d3107298016b212f21f174ef7
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-09-27 13:09:03 +0530

    fixing route

--- src/Utils/Authentication.js
+++ src/Utils/Authentication.js
@@ -7,19 +7,28 @@ export const Authentication = ({ children }) => {
     return authentication ? children : <Navigate to="/login" />;
 }
 export const AuthRoute = ({ children }) => {
+    // const { authentication } = useSelector((state) => state.user);
+    // const navigate = useNavigate();
+
+    // const navigateUser = useCallback(() => {
+    //     navigate(-1);
+    // }, [navigate])
+    // useEffect(() => {
+    //     if (authentication) {
+    //         // Navigate back if authenticated
+    //         navigateUser()
+    //     }
+    // }, [authentication, navigate,navigateUser]);
+
+    // // If not authenticated, render children or redirect to login
+    // return !authentication ? children : navigateUser();
     const { authentication } = useSelector((state) => state.user);
-    const navigate = useNavigate();
 
-    const navigateUser = useCallback(() => {
-        navigate(-1);
-    }, [navigate])
-    useEffect(() => {
-        if (authentication) {
-            // Navigate back if authenticated
-            navigateUser()
-        }
-    }, [authentication, navigate,navigateUser]);
+    // If the user is authenticated, redirect them to the home page or another appropriate page
+    if (authentication) {
+        return <Navigate to="/" replace />;
+    }
 
-    // If not authenticated, render children or redirect to login
-    return !authentication ? children : navigateUser();
+    // If not authenticated, render the children components (e.g., Login or Register)
+    return children;
 }
